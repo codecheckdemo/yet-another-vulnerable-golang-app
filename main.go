0bf546e2199fc4b7b1a2d5a1423067f9d1414bce
张运航 <holic@binance.com>
2019-11-26 16:45:39 +0800

    new

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
-
+    "os"
+    "os/exec"
+    "crypto/rand"
 	"github.com/BurntSushi/toml"
 	"github.com/hashicorp/consul/logger"
 	"github.com/rs/cors"
@@ -21,4 +23,15 @@ func main() {
 	log.Printf(foo.Bar)
 	_ = cors.Options{}
 	_ = logger.LevelFilter()
+    good, _ := rand.Read(nil)
+	println(good)
+	run := "sleep" + os.Getenv("SOMETHING")
+	cmd := exec.Command(run, "5")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Waiting for command to finish...")
+	err = cmd.Wait()
+	log.Printf("Command finished with error: %v", err)
 }
